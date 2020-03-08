fd36531fc174a4306c02da4db8bd2c0499067d8d
I3OR2A <ioariroi@gmail.com>
2020-03-08 17:00:32 +0800

    4-9 商家入驻流程（2）

--- dianping/src/main/java/com/imooc/dianping/controller/admin/SellerController.java
+++ dianping/src/main/java/com/imooc/dianping/controller/admin/SellerController.java
@@ -3,13 +3,20 @@ package com.imooc.dianping.controller.admin;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.imooc.dianping.common.AdminPermission;
+import com.imooc.dianping.common.BusinessException;
+import com.imooc.dianping.common.CommonUtil;
+import com.imooc.dianping.common.EmBusinessError;
 import com.imooc.dianping.model.SellerModel;
+import com.imooc.dianping.request.SellerCreateReq;
 import com.imooc.dianping.service.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller("/admin/seller")
@@ -29,5 +36,30 @@ public class SellerController {
         modelAndView.addObject("CONTROLLER_NAME","seller");
         modelAndView.addObject("ACTION_NAME","index");
         return modelAndView;
+    }
+
+    @RequestMapping("/createpage")
+    @AdminPermission
+    public ModelAndView createPage(){
+        ModelAndView modelAndView = new ModelAndView("/admin/seller/create.html");
+        modelAndView.addObject("CONTROLLER_NAME","seller");
+        modelAndView.addObject("ACTION_NAME","create");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/create",method = RequestMethod.POST)
+    @AdminPermission
+    public String create(@Valid SellerCreateReq sellerCreateReq, BindingResult bindingResult) throws BusinessException {
+        if(bindingResult.hasErrors()){
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, CommonUtil.processErrorString(bindingResult));
+        }
+
+        SellerModel sellerModel = new SellerModel();
+        sellerModel.setName(sellerCreateReq.getName());
+        sellerService.create(sellerModel);
+
+        return "redirect:/admin/seller/index";
+
+
     }
 }
